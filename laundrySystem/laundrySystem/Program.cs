4cf55daf3cc776aa920622de96c98f88840518c5
ChaseJace <jacelopez1527@gmail.com>
2024-03-11 17:40:14 +0800

    created customerForm
    need inventory form

--- laundrySystem/laundrySystem/Program.cs
+++ laundrySystem/laundrySystem/Program.cs
@@ -16,7 +16,7 @@ namespace laundrySystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new loginForm());
+            Application.Run(new customerForm());
         }
     }
 }
